ed792f6ec5162c1cbced5114c1a32cb6e408c6ec
username-u <sopranoluzhin@gmail.com>
2023-01-14 22:45:51 +0200

    refactoring

--- backend_birds/index.js
+++ backend_birds/index.js
@@ -1,12 +1,31 @@
-const app = require('./app')
+const dronesRouter = require('./controllers/droneController')
+const pilotsRouter = require('./controllers/pilotController')
+const express = require('express')
+const cors = require('cors')
 const http = require('http')
-const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+const { cronScan, cronDelete } = require('./utils/cronTasks')
 
-const server = http.createServer(app)
+const app = express()
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use('/api/drones', dronesRouter)
+app.use('/api/pilots', pilotsRouter)
+
+cronDelete()
+cronScan()
+
+app.use(middleware.unknownEndpoint)
+
+app.use(middleware.errorHandler)
 
 const PORT = process.env.PORT || 3003
+const server = http.createServer(app)
 
 server.listen(PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
